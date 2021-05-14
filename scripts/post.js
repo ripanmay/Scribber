aa7903ab6f61650183e59f054ed499568b35fbff
ripansharma <ripan.may@gmail.com>
2021-05-14 23:29:36 +0530

    updated text

--- scripts/post.js
+++ scripts/post.js
@@ -2,7 +2,7 @@ var likeCounter = 0;
 var titleNode = document.getElementById("postTitle");
 var authorNode = document.getElementById("author");
 var contentNode = document.getElementById("postContent");
-var authorContent = 'Srishti Gupta';
+var authorContent = 'Ripan Sharma';
 var titleContent = "Everything you should know about 'module' & 'require' in Node.js";
 var postsContent = 'Node.js treats each JavaScript file as a separate module. For instance, if you have a file containing some code and this file is named xyz.js, then this file is treated as a module in Node';
 
